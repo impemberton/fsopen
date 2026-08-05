2d0379d38d373f06806ea12e762584d2bc9af039
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-05 11:12:36 +0100

    complete part 2.2 and 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -9,11 +9,15 @@ const Course = ({course}) => {
   )
 } 
 
-const Content = ({parts}) => (
+const Content = ({parts}) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return (
   <div>
     {parts.map((part) => <Part key={part.id} part={part} />)}
+    <b>total of {total} exercises</b>
   </div>
-)
+  )
+}
 
 const Part = (props) => (
   <p>
