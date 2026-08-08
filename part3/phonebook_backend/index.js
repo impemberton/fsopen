02f321ce2265700a6071ee132c7e85463990170b
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-08 19:02:56 +0100

    3.7 add morgan middleware

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 
 
 let persons = [
@@ -26,6 +28,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
   response.send('<h1>test</h1>')
