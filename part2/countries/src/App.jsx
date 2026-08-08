8d5a75c34aeccd09bed1dc92bed29dd96cc4ddf4
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-08 15:01:49 +0100

    2.20 weather report for capitals

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const Countries = ({countries, setCountries}) => {
+const api_key = import.meta.env.VITE_WEATHER_API
+
+const Countries = ({countries, setCountries, weather}) => {
         const handleShow = (country) => {
           setCountries([country])
         }
-  
+
         if(countries.length > 10) {
           return (<p>Too many matches, specify another filter</p>)
         }
@@ -24,6 +26,15 @@ const Countries = ({countries, setCountries}) => {
                 {Object.values(country.languages).map(name => <li key={name}>{name}</li>)}
               </ul>
               <img src={country.flags.png} alt={country.flags.alt}/>
+              {
+                weather ?
+                  (<>
+                  <h2>Weather in {country.capital[0]}</h2>
+                  <p>Temperature {weather.main.temp} Celcius</p>
+                  <img src={`https://openweathermap.org/payload/api/media/file/${weather.weather[0].icon}.png`}/>
+                  <p>Wind {weather.wind.speed} m/s</p></>)
+                : null
+              }
             </div>
           )
         }
@@ -35,28 +46,37 @@ const Countries = ({countries, setCountries}) => {
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [weather, setWeather] = useState(null)
   const handleChange = event => {
     if ( event.target.value.length <= 0 ) {
       setCountries([])
     } 
     else {
-    axios
-      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then(response => {
-        const exactFiltered = response.data.filter(country => country.name.common.toLowerCase() === event.target.value.toLowerCase())
-        if (exactFiltered.length === 1) {
-          setCountries(exactFiltered)
-        } else {
-          setCountries(response.data.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())))
-        }
-      })
+      axios
+        .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+        .then(response => {
+          const exactFiltered = response.data.filter(country => country.name.common.toLowerCase() === event.target.value.toLowerCase())
+          if (exactFiltered.length === 1) {
+            setCountries(exactFiltered)
+          } else {
+            setCountries(response.data.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())))
+          }
+        })
+      if (countries.length === 1) {
+        console.log("getting weather")
+        axios
+          .get(
+              `https://api.openweathermap.org/data/2.5/weather?q=${countries[0].capital[0]}&units=metric&appid=${api_key}`
+              )
+          .then(response => {setWeather(response.data)})
+      } 
     }
   }
 
   return (
     <div>
       <p>find countries<input onChange={handleChange}></input></p>
-      <Countries countries={countries} setCountries={setCountries}/>
+      <Countries countries={countries} setCountries={setCountries} weather={weather}/>
     </div>
   )
 }
