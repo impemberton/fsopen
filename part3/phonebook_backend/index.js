428b1dd97b37668ce963ac667622018d67b9b8ca
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-08 18:12:51 +0100

    3.5 implement posting phonebook entries

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -46,6 +46,34 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+const generateId = () => {
+  return (String(Math.floor(Math.random() * 1000000000)))
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing',
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  
+  persons = persons.concat(person)
+  response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter((person) => person.id !== id)
