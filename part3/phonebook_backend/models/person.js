337bc4be0aeb15fc74e4314386d616069d3ee50e
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-09 20:16:58 +0100

    3.20 add validation for numbers

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -19,7 +19,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: { 
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: number => {
+        return /^\d{2,3}-\d+$/.test(number)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, "Phone number is required"]
+  }
 })
 
 personSchema.set('toJSON', {
