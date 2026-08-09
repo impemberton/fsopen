90eaa00560f7fe9a980908fbad95975261b52989
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-09 19:29:14 +0100

    3.16/17 error handling and updating existing entries

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -87,6 +87,24 @@ app.post('/api/people', (request, response) => {
   })
 })
 
+app.put('/api/people/:id', (request, response, next) => {
+  const { name, number } = request.body
+  
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      
+      person.number = number
+
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/people/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
