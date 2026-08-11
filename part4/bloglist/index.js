022b29be2d898666480742d1dd2a5df103dca1b2
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-11 13:00:50 +0100

    4.2 refactor bloglist into separate modules

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,39 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const config = require('./utils/config')
+const app = require('./app')
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
