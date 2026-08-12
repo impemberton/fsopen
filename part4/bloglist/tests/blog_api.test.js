5ffceff8b58b2f69e0f8d554ea7332e1486c2628
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-12 13:13:28 +0100

    4.10 test posting a new blog and refactor the endpoint to use async await

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -26,10 +26,33 @@ test('correct amount of initial blogs are returned', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
-test.only('blog posts contain id property', async () => {
+test('blog posts contain id property', async () => {
   const response = await api.get('/api/blogs')
   
   assert("id" in response.body[0])
+})
+
+test('posting successfully creates a new blog', async () => {
+  const newBlog = {
+    title: "Gobbledygook",
+    author: "Chimmeny Bingus",
+    url: "https://Chimmenybingus.com/",
+    likes: 6,
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const updatedBlogs = await api.get('/api/blogs')
+  console.log(updatedBlogs.body)
+  
+  assert.strictEqual(updatedBlogs.body.length, helper.initialBlogs.length + 1)
+  assert.strictEqual(updatedBlogs.body.at(-1).title, newBlog.title)
+  assert.strictEqual(updatedBlogs.body.at(-1).author, newBlog.author)
+  assert.strictEqual(updatedBlogs.body.at(-1).url, newBlog.url)
+  assert.strictEqual(updatedBlogs.body.at(-1).likes, newBlog.likes)
 })
 
 after(async () => {
