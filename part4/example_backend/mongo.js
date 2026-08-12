7bafd430432f3e050d843e5564883f5e2aae8291
iketyke <isaac.mellad.pemberton@gmail.com>
2026-08-12 12:32:52 +0100

    4.8 test blog api returns correct amount of blogs

--- part4/example_backend/mongo.js
+++ part4/example_backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fsopen:${password}@cluster0.qaaw9p7.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fsopen:${password}@cluster0.qaaw9p7.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url, { family: 4 })
@@ -19,17 +19,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+
+const note = new Note({
+  content: 'Javascript is easy',
+  important: true,
+})
+note.save().then(result => {
+  console.log('note saved!')
   mongoose.connection.close()
 })
-//const note = new Note({
-//  content: 'HTML is easy',
-//  important: true,
-//})
-//note.save().then(result => {
-//  console.log('note saved!')
+
+//Note.find({}).then(result => {
+//  result.forEach(note => {
+//    console.log(note)
+//  })
 //  mongoose.connection.close()
 //})
